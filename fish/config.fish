361dd82052706e31a70cb53f24bcacbfceecc6e7
Tobias Stier <tobiasstier@Tobiass-MacBook-Pro.local>
2019-11-25 15:56:33 +0100

    more fish, more skhd

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,9 @@
 export VIMINIT='let $MYVIMRC="~/.config/vim/vimrc" | source $MYVIMRC'
+
+#fisher bundle plugin
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
