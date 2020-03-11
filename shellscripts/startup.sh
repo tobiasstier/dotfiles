744946d1b4e9fa8258f6e1a7044b0887a2d17291
Tobias Stier <tobiasstier@Tobiass-MacBook-Pro.local>
2020-03-11 23:05:23 +0100

    updated bash profile symlinking

--- shellscripts/startup.sh
+++ shellscripts/startup.sh
@@ -9,14 +9,17 @@ brew install emacs-plus
 ## link emacs app to applications folder
 ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications/Emacs.app
 
+## link bash_profile to $HOME
+ln -s ~/.config/bash/bash_profile ~/.bash_profile
+source ~/.bash_profile
 
 echo "Setting up useful brew installations"
 ## brew installations
 brew install htop jq trash osx-cpu-temp tmux
 brew tap koekeishiya/formulae:
 brew install --HEAD yabai
-sudo yabai --install-sa
 brew cask install bettertouchtool kitty qutebrowser
 brew services start
 
 
+sudo yabai --install-sa
